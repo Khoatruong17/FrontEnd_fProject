5cd07546dfd76c3c19784eaa9bf65b15a47efd8f
Khoatruong17 <truongndkgch190486@fpt.edu.vn>
2024-09-25 15:29:34 +0700

    ok

--- rent-nest/src/App.js
+++ rent-nest/src/App.js
@@ -1,17 +1,24 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './pages/Login.js';
-import Register from './pages/Register.js';
-import AdminDashboard from './pages/AdminDashboard.js';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/admin" element={<AdminDashboard />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
